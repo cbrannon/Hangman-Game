e669d1d13a7af81fec5f122a0de3aa2793513bc7
Cody Brannon <codydbrannon@gmail.com>
2017-02-08 01:35:37 -0500

    Built layout, some styling. Started building game object.

---
+++ assets/javascript/script.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+  var questions = {
+    { question: "Fiat",
+      answer: "Yatta Yatta",
+      imgLink: ""
+    },
+    { question: "Fiat",
+      answer: "Yatta Yatta",
+      imgLink: ""
+    },
+    { question: "Fiat",
+      answer: "Yatta Yatta",
+      imgLink: ""
+    },
+    { question: "Fiat",
+      answer: "Yatta Yatta",
+      imgLink: ""
+    },
+    { question: "Fiat",
+      answer: "Yatta Yatta",
+      imgLink: ""
+    },
+  };
+
+  var Game = function(wins, word, guesses) {
+    this.wins = wins;
+    this.word = word;
+    this.guesses = guesses;
+  }
+
+
+
+
+
+
+
+});
