e26c9b26f04532893c3e843c9465eb7597de1b5b
Cody Brannon <codydbrannon@gmail.com>
2017-02-08 15:26:16 -0500

    Wrote part of the game object.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,37 +1,47 @@
 $( document ).ready(function() {
+  var newGame;
   var questions = {
-    { question: "Fiat",
-      answer: "Yatta Yatta",
-      imgLink: ""
-    },
-    { question: "Fiat",
-      answer: "Yatta Yatta",
-      imgLink: ""
-    },
-    { question: "Fiat",
-      answer: "Yatta Yatta",
-      imgLink: ""
-    },
-    { question: "Fiat",
-      answer: "Yatta Yatta",
-      imgLink: ""
-    },
-    { question: "Fiat",
-      answer: "Yatta Yatta",
-      imgLink: ""
-    },
+    1: { question: "Who dat?",
+         answer: "Me",
+         imgLink: "",
+         audio: "",
+       },
+    2: { question: "Who are you?",
+         answer: "dog",
+         imgLink: "",
+         audio: "",
+       },
   };
 
-  var Game = function(wins, word, guesses) {
-    this.wins = wins;
-    this.word = word;
-    this.guesses = guesses;
+  function Game() {
+    this.wins = 1;
+    this.guesses = [1];
+    this.question = "A";
+    this.answer = "A";
+
+    function addWin() {
+      return this.wins = wins + 1;
+    }
+
+    function setQuestion() {
+      var question = questions[Object.keys(questions)[Math.floor(Math.random()*Object.keys(questions).length)]];
+      this.question = question["question"];
+      this.answer = question["answer"];
+      this.image = question["imgLink"];
+      this.audio = question["audio"];
+    }
+
+    function addGuess(guess) {
+      this.guesses.push(guess);
+    }
   }
 
+  function startGame() {
+    newGame = new Game();
+  }
 
+  startGame();
+  newGame.setQuestion;
 
-
-
-
-
+  console.log(newGame.question);
 });
